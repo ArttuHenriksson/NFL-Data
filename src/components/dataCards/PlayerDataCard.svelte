a74e4cd2db54db99cbed9bc62560f4f64c140030
Arttu Henriksson <ad1820@student.jamk.fi>
2025-01-09 15:03:21 +0200

    perus joo

--- src/components/dataCards/PlayerDataCard.svelte
+++ src/components/dataCards/PlayerDataCard.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import type { Player } from '../../types/players';
   import { getPlayers } from '../../services/dataService';
-
   let players: Player[] = [];
 
   const fetchPlayers = async () => {
@@ -16,15 +15,14 @@
 </script>
 
 <main>
-  <div>
-    <ul>
-      {#each players as player}
-        <li class="text-blue-200 text-center mt-3">
-          {player.first_name}
-          {' '}{player.last_name}
-        </li>
-        <p>{player.position}</p>
-      {/each}
-    </ul>
+  <div class="grid grid-cols-1 gap-4 mt-12 justify-center text-center">
+    {#each players as player}
+      <div>
+        <h5 class="font-bold">{player.first_name} {player.last_name}</h5>
+        <hr />
+        <p>Position: {player.position}</p>
+        <p>College: {player.college}</p>
+      </div>
+    {/each}
   </div>
 </main>
